0552df38850857bc81addcfa3daf65d11b8577d2
dimplep04906 <dimple.panchal@qilegal.com>
2025-09-04 17:45:31 +0530

    string calculator

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,30 +1,41 @@
 class StringCalculator {
-  int add(String numbers) {
-    if (numbers.isEmpty) return 0;
+  int? add(String? numbers) {
+
+    ///1. If number is null or empty = return 0
+    if (numbers == null || numbers.isEmpty) return 0;
 
     String nums = numbers;
+    ///2. for comma and new line
     List<String> delimiters = [',', '\n'];
 
+    ///3. custom delimiter
     if (nums.startsWith('//')) {
       final index = nums.indexOf('\n');
-      final delim = nums.substring(2, index);
-      delimiters = [delim];
+      final delimit = nums.substring(2, index);
       nums = nums.substring(index + 1);
+      delimiters = [delimit];
     }
 
+
+    ///4. split numbers into delimiters
     final pattern = delimiters.map(RegExp.escape).join('|');
     final parts = nums.split(RegExp(pattern));
-    final ints = parts.where((p) => p.isNotEmpty).map(int.parse).toList();
 
-    final negatives = ints.where((i) => i < 0).toList();
+
+    ///5. convert into int
+    final numbersList = <int>[];
+    for (final p in parts) {
+      if (p.trim().isEmpty) continue;
+      numbersList.add(int.parse(p));
+    }
+
+    ///6. check negatives
+    final negatives = numbersList.where((n) => n < 0).toList();
     if (negatives.isNotEmpty) {
       throw FormatException('Negative numbers not allowed: ${negatives.join(',')}');
     }
-
-    return ints.fold(0, (a, b) => a + b);
+    ///return sum
+    return numbersList.fold(0, (a , b) => a! + b);
 
   }
-
-
-
 }
--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -5,13 +5,20 @@ void main() {
   final calc = StringCalculator();
 
   test('empty string returns 0', () => expect(calc.add(''), 0));
+
   test('two numbers seperated by comma', () => expect(calc.add('1,5'), 6));
+
   test('multiple numbers', () => expect(calc.add('1,2,3,4'), 10));
+
   test('support newline delimiter', () => expect(calc.add('1\n2,3'), 6));
+
   test('custom single-char delimiter', () => expect(calc.add('//;\n1;2'), 3));
+
   test('negative numbers', () => expect(() => calc.add('1,-2,3,-4'),
       throwsA(isA<FormatException>().having((e) => e.message, 'message',
           'Negative numbers not allowed: -2,-4'))));
+
+
 
 
 }
